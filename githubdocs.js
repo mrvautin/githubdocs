5c971aad6acc8ce0dbad4a2e651243f72a1698f6
Mark Moffat <mark@dimebox.com>
2017-09-04 14:32:51 +0200

    More logging

--- githubdocs.js
+++ githubdocs.js
@@ -11,11 +11,11 @@ if(args.length > 0){
 // Build docs
 if(args === 'build'){
     shared.build(function(){
-        console.log("Successfully built");
+        console.log("[INFO] Successfully built");
         process.exit(0);
     });
 }else if(args === 'serve'){
     require("./server")
 }else{
-    console.log("No option selected. Valid options are: build, serve");
+    console.log("[ERROR] No option selected. Valid options are: build, serve");
 }
